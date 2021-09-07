6505271e81c82a0779832c1a32dc828bdba6b10e
Patrek Gill <patrekgill@yahoo.com>
2021-09-07 09:09:42 -0700

    complete objective7lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,13 @@
+class Objective7Lab4 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int count = 1;
+
+        while (count <= 20) {
+            sum += count;
+            count++;
+        }
+
+        System.out.println(sum);
+    }
+}
